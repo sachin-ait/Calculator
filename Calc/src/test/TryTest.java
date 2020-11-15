f5ab6e336c42e611b4f315c8025f3bee6518375e
Som279807 <72744729+Som279807@users.noreply.github.com>
2020-11-15 19:27:40 +0000

    Added JUnit test cases
    Added JUnit test cases based on user stories and acceptance criteria

--- Calc/src/test/TryTest.java
+++ Calc/src/test/TryTest.java
@@ -21,39 +21,51 @@ public class CalculatorTest extends TestCase { //New Class for all tests
    public CalculatorTest(String name) {
    super(name);
    }
- 	public void testSumPosNum() { //User story CAL1 a acceptance criteria
+	@Test
+	public void testSumPosNum() { //User story CAL1 a acceptance criteria
    assertEquals(2,cal.sum(1,1));
   }
+	@Test
 	public void testSumNegNum() { //User story CAL1 b acceptance criteria
    assertEquals(-2,cal.sum(-4,-2));
   }
+	@Test
 	public void testSumMixNum() { //User story CAL1 c acceptance criteria
    assertEquals(5,cal.sum(15,-10));
   }
+	@Test
 	public void testSubPosNum() { //User story CAL2 a acceptance criteria
    assertEquals(-1,cal.sub(5,4));
   }
+	@Test
 	public void testSubNegNum() { //User story CAL2 b acceptance criteria
    assertEquals(3,cal.sub(-3,-6));
   }
+	@Test
 	public void testSubMixNum() { //User story CAL2 c acceptance criteria
    assertEquals(21,cal.sub(14,-7));
   }
+	@Test
 	public void testMulPosNum() { //User story CAL3 a acceptance criteria
    assertEquals(9,cal.mul(3,3));
   }
+	@Test
 	public void testMulNegNum() { //User story CAL3 b acceptance criteria
    assertEquals(4,cal.mul(-2,-2));
   }
+	@Test
 	public void testMulMixNum() { //User story CAL3 c acceptance criteria
    assertEquals(-8,cal.mul(-4,2));
   }
+	@Test
 	public void testDivPosNum() { //User story CAL4 a acceptance criteria
    assertEquals(6,cal.div(12,2));
   }
+	@Test
 	public void testDivNegNum() { //User story CAL4 b acceptance criteria
    assertEquals(20,cal.div(-40,-2));
   }
+	@Test
 	public void testDivMixNum() { //User story CAL4 c acceptance criteria
    assertEquals(-13,cal.div(-26,2));
   }
