090a0c03d230a5c479acbc545e13ac45a76196cf
sachin <A00279809@student.ait.ie>
2020-12-03 20:25:31 +0000

    exception

---
+++ Calc/src/code/InvalidExpression.java
@@ -0,0 +1,9 @@
+package code;
+
+public class InvalidExpression extends Exception{
+	@Override
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return "Invalid Expression";
+	}
+}
